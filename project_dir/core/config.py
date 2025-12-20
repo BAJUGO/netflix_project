89ee40a697b7f077774c86c25cd51022065bfb75
Dima <texetbuy@gmail.com>
2025-12-20 15:04:25 +0300

    db_helper

--- project_dir/core/config.py
+++ project_dir/core/config.py
@@ -4,3 +4,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     db_url: str = "postgresql+asyncpg://dima:kiril12AZ@localhost:5432/netflix"
     echo: bool = True
+
+
+settings = Settings()
---
+++ project_dir/core/db_helper.py
@@ -0,0 +1,34 @@
+from asyncio import current_task
+
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
+
+from ..core import settings
+
+
+class DbHelper:
+    def __init__(self, url: str, echo: bool):
+        self.engine = create_async_engine(url=url, echo=echo)
+
+        self.session_factory=async_sessionmaker(engine=self.engine,
+                                                autroflush=False,
+                                                autocommit=False,
+                                                expire_on_commit=False)
+
+
+    def get_scoped_session(self):
+        return async_scoped_session(session_factory=self.session_factory,
+                                    scopefunc=current_task)
+
+
+    async def session_dependency(self):
+        session = self.get_scoped_session()
+        async with session() as sess:
+            yield sess
+
+
+db_helper = DbHelper(url=settings.db_url, echo=settings.echo)
