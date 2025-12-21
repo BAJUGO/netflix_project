8adcbc4a3ead3334153a6fa7cd971f1c08d5cdca
Dima <texetbuy@gmail.com>
2025-12-21 14:56:11 +0300

    LOTS OF CRUUUUD!

--- project_dir/views_part/crud.py
+++ project_dir/views_part/crud.py
@@ -1,21 +1,63 @@
-from fastapi import Depends
+from fastapi import HTTPException
+from pydantic import BaseModel
+from sqlalchemy import Select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .schemas import AuthorCreate, MovieCreate
-from ..core import db_helper
-from ..models import Author, Movie
+from ..models import Author, Movie, Series
 
-async def add_author_session(author_in: AuthorCreate, session: AsyncSession):
-    author = Author(**author_in.model_dump())
-    session.add(author)
+SCHEMAS_CLS = {
+    "AUTHOR": Author,
+    "MOVIE": Movie,
+    "SERIES": Series
+}
+
+
+async def adder_session(data: BaseModel, session: AsyncSession, schema: str):
+    cls = SCHEMAS_CLS[schema]
+    obj = cls(**data.model_dump())
+    session.add(obj)
     await session.commit()
-    await session.refresh(author)
-    return author
+    await session.refresh(obj)
+    return obj
 
 
-async def add_movie_session(movie_in: MovieCreate, session: AsyncSession):
-    movie = Movie(**movie_in.model_dump())
-    session.add(movie)
+async def getter_session(session: AsyncSession, schema_name: str):
+    cls = SCHEMAS_CLS[schema_name]
+    stmt = Select(cls).order_by(cls.id)
+    result = await session.scalars(stmt)
+    return list(result)
+
+
+async def deleter_session(session: AsyncSession, obj_id: int, schema_name: str, ):
+    cls = SCHEMAS_CLS[schema_name]
+    obj_to_del = await session.get(cls, obj_id)
+    if obj_to_del is None:
+        raise HTTPException(status_code=404, detail=f"{schema_name.lower()} wasn't found")
+    await session.delete(obj_to_del)
     await session.commit()
-    await session.refresh(movie)
-    return movie
+    return {f"{schema_name} has been deleter"}
+
+
+async def delete_author_session(session: AsyncSession, obj_id: int):
+    return await deleter_session(session, obj_id, "AUTHOR")
+
+
+async def add_author_session(author_in: AuthorCreate, session: AsyncSession) -> Author:
+    return await adder_session(author_in, session, "AUTHOR")
+
+
+async def add_movie_session(movie_in: MovieCreate, session: AsyncSession) -> Movie:
+    return await adder_session(movie_in, session, "MOVIE")
+
+
+async def get_authors_session(session: AsyncSession) -> list[Author]:
+    return await getter_session(session, "AUTHOR")
+
+
+async def get_movies_session(session: AsyncSession) -> list[Movie]:
+    return await getter_session(session, "MOVIE")
+
+
+async def get_series_session(session: AsyncSession) -> list[Series]:
+    return await getter_session(session, "SERIES")
--- project_dir/views_part/routers.py
+++ project_dir/views_part/routers.py
@@ -2,17 +2,35 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from project_dir.core.db_helper import db_helper
 
-from project_dir.views_part.crud import add_author_session, add_movie_session
+from project_dir.views_part.crud import add_author_session, add_movie_session, get_authors_session, get_movies_session, \
+    delete_author_session
 from project_dir.views_part.schemas import AuthorCreate, AuthorSchema, MovieSchema, MovieCreate
 
 router = APIRouter()
 
+ses_dep = Depends(db_helper.session_dependency) # type = AsyncSession
 
 @router.post("/add_author", response_model=AuthorSchema)
-async def add_author(author_in: AuthorCreate, session: AsyncSession = Depends(db_helper.session_dependency)):
+async def add_author(author_in: AuthorCreate, session: AsyncSession = ses_dep):
     return await add_author_session(author_in, session)
 
 
 @router.post("/add_movie", response_model=MovieSchema)
-async def add_movie(movie_in: MovieCreate, session: AsyncSession = Depends(db_helper.session_dependency)):
+async def add_movie(movie_in: MovieCreate, session: AsyncSession = ses_dep):
     return await add_movie_session(movie_in, session)
+
+
+@router.get("/get_authors", response_model=list[AuthorSchema])
+async def get_authors(session: AsyncSession = ses_dep):
+    return await get_authors_session(session)
+
+
+@router.get("/get_movies", response_model=list[MovieSchema])
+async def get_movies(session: AsyncSession = ses_dep):
+    return await get_movies_session(session)
+
+
+@router.delete("/{author_id}")
+async def delete_author(author_id: int, session: AsyncSession = ses_dep):
+    return await delete_author_session(session, author_id)
+
