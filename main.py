2ef9eb5a15dd580b875eb5dd9a86f6d5164ddb6e
Dima <texetbuy@gmail.com>
2026-01-04 19:57:21 +0300

    routers move

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 from project_dir.loging_and_exc import lifespan, do_middleware, custom_exception_handler
-from project_dir.views_part import routers_router
+from project_dir.routers.get_post_router import router as get_post_router
+from project_dir.routers.delete_put_patch_router import router as delete_put_patch_router
 
 app = FastAPI(lifespan=lifespan)
-
-app.include_router(routers_router)
-
 app.middleware("http")(do_middleware)
+app.include_router(get_post_router)
+app.include_router(delete_put_patch_router)
+
+
 
 custom_exception_handler(app)
 
