b4306d65591aad261677e0a623b6385ae889ec3e
Dima <texetbuy@gmail.com>
2026-01-17 11:10:39 +0300

    Directory ordering Adders work

--- html/html_scripts/adders/funcs_adders.js
+++ html/html_scripts/adders/funcs_adders.js
@@ -1,5 +1,5 @@
-import {mapping} from "./funcs_getters.js";
-import {json_fetch} from "./funcs.js";
+import {mapping} from "../funcs_getters.js";
+import {json_fetch} from "../funcs.js";
 
 
 export async function add_object(event, object_type, list_of_attrs) {
