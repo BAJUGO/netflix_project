c12ff544f93fb150b7dfcbabc2699510e1293e9a
Dima <texetbuy@gmail.com>
2026-01-17 10:38:57 +0300

    Improved display of users list

--- html/html_scripts/users.js
+++ html/html_scripts/users.js
@@ -8,9 +8,8 @@ document.getElementById("get_all_users_button").addEventListener('click', event=
     json_fetch("http://localhost:8000/users/get_users", {credentials: "include"}).then(response => {
         for (let user in response) {
             let new_li = document.createElement("li")
-            new_li.innerText = response[user]
+            new_li.innerText = `Visible name = ${response[user][0]}, ID = ${response[user][2]}, role = ${response[user][1]}`
             ul.appendChild(new_li)
-
         }
         console.log(response)
     })
